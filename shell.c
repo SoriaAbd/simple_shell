19b001137481c402295747952f9a8dc6b8597203
SoriaAbd <soriaabd1994@gmail.com>
2023-11-15 01:53:46 -0800

    0

---
+++ shell.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+/*
+ * main - first call function
+ * @argc: num of argument
+ * @argv: list of argument
+ * Return:intger
+ */
+int main(int argc, char **argv)
+{
+	int i = 0, s = 0;/*i for index and s for status*/
+	char **com, *l = NULL;/*command and line*/
+	(void)argc;
+
+	while (1)
+	{}
+}
---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif
